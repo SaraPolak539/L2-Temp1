64d75bb386555794c09d02cdb03cc2b7f1832d7b
SaraPolak539 <sara@realit.org>
2021-02-08 17:00:02 +0200

    comment - add file TestingGit.java mistake correction in Harmonic.java and Person.java

---
+++ L2-temp/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main( String[] args ) {
+    	//Math a= new Math()
+    
+        // accessing the methods of the Math class
+        System.out.println("Absolute value of -12 =  " + Math.abs(-12));
+        System.out.println("Value of PI = " + Math.PI);
+        System.out.println("Value of E = " + Math.E);
+        System.out.println("2^2 = " + Math.pow(2,2));
+    }
+}
